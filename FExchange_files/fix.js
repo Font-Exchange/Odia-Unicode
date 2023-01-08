4e44d06091af83d5dbd48499be8408818bf634b0
Font-Exchange <94764286+Font-Exchange@users.noreply.github.com>
2023-01-09 02:45:50 +0530

    Update fix.js

--- FExchange_files/fix.js
+++ FExchange_files/fix.js
@@ -2,7 +2,10 @@
     $('#btnfix').click(function() {
     var textarea=$('#Unicode'); 
 
-textarea.val(textarea.val().replace(/ର୍ୟ୍ୟ/g,"ର୍ଯ୍ୟ")); 
+textarea.val(textarea.val().replace(/ର୍ୟ୍ୟ/g,"ର୍ଯ୍ୟ"));
+textarea.val(textarea.val().replace(/ତ୍ଵ/g,"ତ୍ବ")); 
+textarea.val(textarea.val().replace(/ସ୍ଵ/g,"ସ୍ବ"));
+textarea.val(textarea.val().replace(/ଵ/g,"ବ"));
 
     });
 });
