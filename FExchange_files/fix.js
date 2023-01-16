35b42a12431744e410337d661ada322a4c6397b3
Font-Exchange <94764286+Font-Exchange@users.noreply.github.com>
2023-01-16 12:39:25 +0530

    Update fix.js

--- FExchange_files/fix.js
+++ FExchange_files/fix.js
@@ -6,6 +6,7 @@ textarea.val(textarea.val().replace(/ର୍ୟ୍ୟ/g,"ର୍ଯ୍ୟ"));
 textarea.val(textarea.val().replace(/ତ୍ଵ/g,"ତ୍ବ")); 
 textarea.val(textarea.val().replace(/ସ୍ଵ/g,"ସ୍ବ"));
 textarea.val(textarea.val().replace(/ଵ/g,"ବ"));
+textarea.val(textarea.val().replace(/ଡ଼/g,"ଡ଼"));
 
     });
 });
