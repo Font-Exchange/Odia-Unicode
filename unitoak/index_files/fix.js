7f72a25e3fb7bd8da422604433a6a0f2c7d0aa85
Font-Exchange <94764286+Font-Exchange@users.noreply.github.com>
2023-12-14 20:28:50 +0530

    Add files via upload

---
+++ unitoak/index_files/fix.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+    $('#btnfix').click(function() {
+    var textarea=$('#Unicode'); 
+
+textarea.val(textarea.val().replace(/e୍d୍d/g,"ର୍ଯ୍ୟ"));
+textarea.val(textarea.val().replace(/i୍a/g,"Ê")); 
+textarea.val(textarea.val().replace(/\୍~/g,"\¥"));
+textarea.val(textarea.val().replace(/~଼/g,"d"));
+textarea.val(textarea.val().replace(/i୍c/g,"iá"));
+textarea.val(textarea.val().replace(/Kðୋ/g,"ùKðû"));
+textarea.val(textarea.val().replace(/h୍Y/g,"¾"));
+
+
+    });
+});
