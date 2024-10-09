d7f41e428e84ece6f4ed6e2d7605bbdc31ff1b1f
Font-Exchange <94764286+Font-Exchange@users.noreply.github.com>
2024-10-09 12:08:25 +0530

    Delete docx/Docx Converter_files directory

--- docx/Docx Converter_files/main.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.forEach.call(
-    document.querySelectorAll("pre code[class*='language-']"),
-    function(element) {
-        hljs.highlightBlock(element);
-        element.classList.remove("hljs");
-    });
